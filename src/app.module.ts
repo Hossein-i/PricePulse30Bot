8a3b612d2c9ec2814b957401af04b77fbb61c5d0
Hossein-i <20hossein00@gmail.com>
2025-02-06 14:59:58 +0330

    add @nestjs/config dependency and configure ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
-import { ScheduleModule } from "@nestjs/schedule"
-import { TelegramModule } from "./telegram/telegram.module"
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), TelegramModule],
 })
 export class AppModule {}
-
